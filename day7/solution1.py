1641baf67f4374c0eb713ee76907106b10a0db36
Edwin Jones <>
2020-12-07 19:37:53 +0000

    add day 7 part 1

---
+++ day7/solution1.py
@@ -0,0 +1,30 @@
+import helpers
+
+lines = helpers.get_input_as_lines()
+
+def get_result():
+    target_color = "shiny gold"
+    colors = set()
+    colors.add(target_color)
+    processing = True
+
+    while(processing):
+        processing = False
+        for line in lines:
+            if "no other bags" in line:
+                continue
+            
+            pos = line.index(' ')
+            current_color = line[:line.index(' ', pos + 1)]
+            if current_color == target_color:
+                continue
+
+            if any(color in line for color in colors):
+                if(current_color not in colors):
+                    colors.add(current_color)
+                    processing = True
+    
+    return len(colors) - 1
+
+result = get_result()
+print("Result is: " + str(result))
