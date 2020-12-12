210944962d72f6bb25f196d9a6f4803a1193f506
Edwin Jones <>
2020-12-12 20:02:30 +0000

    split logic into more classes day 12

--- day12/solution2.py
+++ day12/solution2.py
@@ -5,6 +5,14 @@ class Position():
   def __init__(self, x=0, y=0):
     self.x = x
     self.y = y
+  def  __add__(self, other):
+    self.x = self.x + other.x
+    self.y = self.y + other.y
+    return self
+  def  __sub__(self, other):
+    self.x = self.x - other.x
+    self.y = self.y - other.y
+    return self
 
 class Ship():
   directions = ['N', 'E', 'S', 'W']
@@ -12,8 +20,15 @@ class Ship():
   def __init__(self):
     self.position = Position()
     self.current_direction_index = 1
-  
-  def apply_move(self, move):
+
+  def __str__(self):
+    return f"Ship. Dir: {self.directions[self.current_direction_index]}, x:{self.position.x}, y:{self.position.y}"
+
+class Waypoint(Ship):
+  def __str__(self):
+    return f"Waypoint. Dir: {self.directions[self.current_direction_index]}, x:{self.position.x}, y:{self.position.y}"
+
+  def apply_move(self, move, ship):
     direction = move[0]
     distance = move[1]
 
@@ -59,9 +74,28 @@ class Ship():
 def get_result():
   moves = helpers.get_input_as_moves()
   ship = Ship()
+  waypoint = Waypoint()
+  waypoint.position.x = 10
+  waypoint.position.y = 1
 
   for move in moves:
-    ship.apply_move(move)
+    print(f"{move}")
+    direction = move[0]
+    distance = move[1]
+    if direction == 'F':
+      diff = waypoint.position - ship.position
+      for _ in range(distance):
+        ship.position = ship.position + diff
+      waypoint.position = waypoint.position + ship.position
+      print(ship)
+      print(waypoint)
+
+    else:
+      if direction == 'R':
+        pass
+      waypoint.apply_move(move, ship)
+      print(waypoint)
+
 
   result = abs(ship.position.x) + abs(ship.position.y)
   return result
