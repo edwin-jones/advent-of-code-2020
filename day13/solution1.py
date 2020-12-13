678e7c4af91945061cea0c5c48b241ca91b970a6
Edwin Jones <>
2020-12-13 12:28:23 +0000

    day 13 part 1

--- day13/solution1.py
+++ day13/solution1.py
@@ -1,13 +1,29 @@
 from helpers import *
 
-
-
 def get_result():
   time = get_departure_time()
   numbers = get_bus_numbers()
-  print(time)
-  print(numbers)
-  return 0
+  results = {}
+
+  for number in numbers:
+    accumulator = 0
+    while True:
+      accumulator += number
+      if accumulator > time:
+        break
+
+    diff = accumulator - time
+    print(f"BUS:{number} WAIT:{diff} ACC:{accumulator}")
+    results[diff] = number
+
+    shortest_wait = 2**32
+    for key in results.keys():
+      if key < shortest_wait:
+        shortest_wait = key
+
+    bus = results[shortest_wait]
+    print(f"RESULT: bus = {bus}, shortest wait:{shortest_wait}")
+  return shortest_wait * bus
 
 result = get_result()
 print("Result is: " + str(result))
