6603c90c7961be314c983412626c47dc21c9c1e9
Edwin Jones <>
2020-12-13 17:52:14 +0000

    remove debug prints

--- day13/solution2.py
+++ day13/solution2.py
@@ -4,7 +4,6 @@ def check_prev_bus_time(bus_num, previous_time, gap):
   result = 0
   while result < previous_time:
     result += bus_num
-  
   if result - gap == previous_time:
     return True
   else:
@@ -19,22 +18,18 @@ def get_result():
 
   while True:
     time_cache = time
-    print(str(time))
     for bus in buses:
       if bus == 'x':
         time = time - 1
         continue
       number = int(bus)
       if time % number == 0:
-        print(f"Bus:{bus}, Time:{time}")
         if(bus == buses[-1]):
           return time
         time = time -1
       else:
         time = time_cache + step
         break
-
-
   return 0
 
 result = get_result()
